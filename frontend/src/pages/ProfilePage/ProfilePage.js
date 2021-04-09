c5baa55523d7b7a8f1248053649ba0f5733974c9
Lewis Rotchell <rotchell92@gmail.com>
2021-04-09 12:59:03 +0100

    added more profile page functionality

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -4,6 +4,8 @@ import { getPosts } from "../../redux/post/postActions";
 import { getUser } from "../../redux/users/usersActions";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
+import { BeatLoader } from "react-spinners";
+import { css } from "@emotion/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import Button from "@material-ui/core/IconButton";
 import Avatar from "@material-ui/core/Avatar";
@@ -37,7 +39,23 @@ const useStyles = makeStyles({
   followItems: {
     padding: "0 6px",
   },
+  changeProfile: {
+    fontSize: "12px",
+    backgroundColor: "#fff",
+    border: "none",
+    outline: "none",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
 });
+
+const loaderCSS = css`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 const ProfilePage = ({ match }) => {
   const classes = useStyles();
@@ -51,8 +69,19 @@ const ProfilePage = ({ match }) => {
   const userInfo = useSelector((state) => state.users.user);
 
   const authInfo = useSelector((state) => state.userLogin.userInfo);
+  const authLoading = useSelector((state) => state.userLogin.loading);
 
-  const { photo, following, followers, username } = userInfo;
+  const { photo, following, followers, username, _id } = userInfo;
+
+  const authFollowing = useSelector(
+    (state) => state.userLogin.userInfo?.following
+  );
+
+  const isFollowing = () => {
+    if (authFollowing) {
+      return authFollowing.find((i) => i === _id);
+    } else return;
+  };
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -76,17 +105,27 @@ const ProfilePage = ({ match }) => {
         <ArrowBackIcon />
       </Button>
 
-      {postsLoading || userLoading ? (
-        <div>LOADING</div>
-      ) : postError ?? userError ? (
+      {postsLoading || userLoading || authLoading ? (
+        <BeatLoader loading css={loaderCSS} />
+      ) : postError || userError ? (
         <div>ERROR!!!!!!!!!!!!!!!!!!!!!!!!</div>
       ) : (
         <div className={classes.profileContainer}>
           {" "}
           <Avatar className={classes.profileImage} src={photo}></Avatar>{" "}
+          {authInfo._id === userInfo._id && (
+            <button className={classes.changeProfile}>
+              Change Profile Picture
+            </button>
+          )}
           <Typography variant="h4" className={classes.titleUsername}>
             {username}
           </Typography>
+          {authInfo._id !== userInfo._id && (
+            <Button size="small" variant="contained" color="primary">
+              {isFollowing() !== undefined ? "Following" : "Follow"}
+            </Button>
+          )}
           <div className={classes.followNums}>
             <Typography className={classes.followItems} variant="body2">
               followers:{followers.length}
